ed2e6d2951dce284581ea4b68d94f36e76c7ffec
Asadullah325 <asadullah456545@gmail.com>
2025-03-18 15:31:50 +0500

    React Router Done

---
+++ react-router/src/main.jsx
@@ -0,0 +1,35 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import Home from "./components/Home";
+import About from "./components/About";
+import ContactUs from "./components/ContactUs";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "",
+        element: <Home />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/contact",
+        element: <ContactUs />,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+);
